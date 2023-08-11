88a052be89e5780bbc736deecfb747ad31aabf0b
HACK-NET-NOIR-SOFTWARES <141960767+HACK-NET-NOIR-SOFTWARES@users.noreply.github.com>
2023-08-11 13:53:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,17 +6,17 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        '/',
-        '/flashbtc/index.html',
-        '/flashbtc/index.css',
-        '/flashbtc/fun.js',
-        '/homescreen72.png',
-        '/homescreen48.png',
-        '/homescreen96.png',
-        '/homescreen144.png',
-        '/homescreen168.png',
-        '/homescreen192.png',
-        '/manifest.json'
+        './FLASHBTC-CORE/',
+        './FLASHBTC-CORE/flashbtc/index.html',
+        './FLASHBTC-CORE/flashbtc/index.css',
+        './FLASHBTC-CORE/flashbtc/fun.js',
+        './FLASHBTC-CORE/homescreen72.png',
+        './FLASHBTC-CORE/homescreen48.png',
+        './FLASHBTC-CORE/homescreen96.png',
+        './FLASHBTC-CORE/homescreen144.png',
+        './FLASHBTC-CORE/homescreen168.png',
+        './FLASHBTC-CORE/homescreen192.png',
+        './FLASHBTC-CORE/manifest.json'
         // Add more URLs to cache as needed
       ]);
     })
